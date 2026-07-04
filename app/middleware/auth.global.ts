668c86f4fa5c3651b5909ad1a4f4b62258515d4e
Jeremy Voros <jeremyvoros@gmail.com>
2026-07-04 12:29:03 -0600

    add '/' route to middleware

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,6 +1,10 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn } = useUserSession();
 
+  if (to.path === "/") {
+    return navigateTo(loggedIn.value ? "/board" : "/login");
+  }
+
   if (!loggedIn.value && to.path !== "/login") {
     return navigateTo("/login");
   }
