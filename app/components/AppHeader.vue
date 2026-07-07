52afd374cfc3620de1f86f6e01ec6e11713504ad
Jeremy Voros <jeremyvoros@gmail.com>
2026-07-07 15:04:31 -0600

    starting on sections and shifts

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <UHeader
         title="Patient Assignment Tool"
-        class="border-0"
+        class="border-0 mb-2 md:mb-10"
         :toggle="{ class: 'hidden' }"
     >
         <template #title>
--- app/pages/board.vue
+++ app/pages/board.vue
@@ -8,7 +8,23 @@ const { board, config, connected } = useBoard();
     <AppHeader />
     <UMain>
         <UContainer>
-            <div class="grid grid-cols-1 border border-red-200">container</div>
+            <div v-if="board" class="grid grid-cols-1 md:grid-cols-12 md:gap-8">
+                <section class="order-1 md:order-2 md:col-span-5">
+                    <SectionRotation :zone="board.zones.main" />
+                </section>
+                <section class="order-2 md:order-3 md:col-span-3">
+                    <template v-for="zoneSlug in board.zoneOrder">
+                        <SectionRotation
+                            v-if="zoneSlug !== 'main'"
+                            :zone="board.zones[zoneSlug]"
+                        />
+                    </template>
+                </section>
+                <section class="order-3 md:order-1 md:col-span-4">
+                    <SectionHeader title="Timeline" />
+                    <div class="mb-4"></div>
+                </section>
+            </div>
         </UContainer>
         <UCollapsible>
             <UButton variant="subtle">Config JSON</UButton>
