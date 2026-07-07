e5e4f599b667a8296fde8ba86b69c12742270ea1
Jeremy Voros <jeremyvoros@gmail.com>
2026-07-07 15:04:40 -0600

    sections

---
+++ app/components/SectionHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+    title?: string;
+    icon?: string;
+}>();
+
+defineEmits(["section-header-action"]);
+</script>
+<template>
+    <UTooltip text="click to collapse">
+        <div
+            class="border-b-1 border-gray-200 flex justify-between items-center cursor-pointer"
+        >
+            <h3 class="font-bold md:text-lg uppercase tracking-wider">
+                {{ title }}
+            </h3>
+            <UIcon v-if="icon" :name="icon" class="size-4" />
+        </div>
+    </UTooltip>
+</template>
---
+++ app/components/SectionRotation.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { Zone } from "../../server/core/types";
+const { board } = useBoard();
+defineProps<{
+    zone?: Zone;
+}>();
+
+const open = ref(true);
+
+function headerClick() {
+    alert("clicked section-header-action");
+}
+</script>
+
+<template>
+    <UCollapsible v-model:open="open" class="mb-2 md:mb-9">
+        <SectionHeader :title="zone.name" icon="lucide:chevrons-up-down" />
+        <template #content>
+            <div
+                v-for="shiftId in zone.shifts"
+                class="border-b-1 border-gray-200"
+            >
+                <div class="text-xs md:text-base">
+                    {{ board.shifts[shiftId].name }}
+                </div>
+                <div class="font-bold">
+                    {{ board.shifts[shiftId].first }}
+                    {{ board.shifts[shiftId].last }}
+                </div>
+            </div>
+            <div class="mb-4">Lorem ipsum</div>
+        </template>
+    </UCollapsible>
+</template>
