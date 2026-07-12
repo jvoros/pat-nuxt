2bb203758b686857814e4a98b1fbd6de94536a64
Jeremy Voros <jeremyvoros@gmail.com>
2026-07-11 21:02:56 -0600

    fast track buttons

--- app/utils/shiftFlags.ts
+++ app/utils/shiftFlags.ts
@@ -15,7 +15,9 @@ export const getShiftFlags = (
   shift: Shift,
 ): ShiftFlags => {
   const isRotating = zone.type === "rotation" || zone.type === "dual";
-  const isNext = isRotating && zone.shifts[zone.next ?? -1] === shiftId;
+  const isNext =
+    (isRotating && zone.shifts[zone.next ?? -1] === shiftId) ||
+    (zone.type === "simple" && zone.shifts[0] === shift.id);
   const isSuper =
     zone.type === "dual" && zone.shifts[zone.super ?? -1] === shiftId;
   const isPaused = isRotating && shift.status === "paused";
